01eddc84d6384eea7d024213b19dd60b3c7c7e58
Ofer Sadgat <ofer.sadgat@enterthemist.com>
2016-01-09 17:57:00 -0800

    fix missing referece

--- src/app.js
+++ src/app.js
@@ -3,16 +3,37 @@ const React = require('react');
 
 const Window = Mist.Window;
 
-var windowJsx = (
-	<Window title="Hello World" 
-			className="hello-world"
-			canResize={true} 
-			initialX={undefined} 
-			initialY={undefined} 
-			initialHeight={600} 
-			initialWidth={850} >
-		HELLO WORLD
-	</Window>
-);
-Mist.WindowManager.addWindow(windowJsx);
+var file = process.argv[0];
+var mimeType = Mist.File.getMimeType(file.name);
+if (mimeType.indexOf('image/') === 0){
+	file.getBinary().then(function(binary){
+		var addWindow = function(base64data){
+			var windowJsx = (
+				<Window title="Image Viewer" 
+						className="image-viewer"
+						canResize={true} 
+						initialX={undefined} 
+						initialY={undefined} 
+						initialHeight={600} 
+						initialWidth={850} >
+					<div style={{
+						'width': '100%',
+						'height': '100%',
+						'backgroundImage':
+							'url(data:' + file.getType() + ';base64,' + base64data + ')',
+					    'backgroundSize': 'contain',
+    					'backgroundRepeat': 'no-repeat',
+					}} />
+				</Window>
+			);
+			Mist.WindowManager.addWindow(windowJsx);
+		};
+		var base64promise = Binary.toBase64(binary);
+		if (base64promise.then){
+			base64promise.then(addWindow);
+		} else {
+			addWindow(base64promise);
+		}
+	});
+}
 
